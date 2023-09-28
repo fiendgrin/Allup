3699f2c79f35dfbaffc93fd1bf0d1f0ae3621f20
Huseynov Yunis <yunisah@code.local>
2023-09-28 16:34:25 +0400

    Update

---
+++ Allup/Allup/Models/Order.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Allup.Models
+{
+    public class Order : BaseEntity
+    {
+        public int No { get; set; }
+        public int? UserId { get; set; }
+        public AppUser? User { get; set; }
+
+        public int Status { get; set; }
+        [StringLength(255)]
+        public string? Comment { get; set; }
+        [StringLength(255)]
+        public string Name { get; set; }
+        [StringLength(255)]
+        public string SurName { get; set; }
+        [StringLength(255),EmailAddress]
+        public string Email { get; set; }
+        [StringLength(255)]
+        public string Line1 { get; set; }
+        [StringLength(255)]
+        public string Line2 { get; set; }
+        [StringLength(255)]
+        public string Country { get; set; }
+        [StringLength(255)]
+        public string Town { get; set; }
+        [StringLength(255)]
+        public string State { get; set; }
+        [StringLength(255)]
+        public string PostalCode { get; set; }
+    }
+}
--- Allup/Allup/Services/LayoutService.cs
+++ Allup/Allup/Services/LayoutService.cs
@@ -2,6 +2,7 @@
 using Allup.Interfaces;
 using Allup.Models;
 using Allup.ViewModels.BasketVMs;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
@@ -11,37 +12,80 @@ namespace Allup.Services
     {
         private readonly AppDbContext _context;
         private readonly IHttpContextAccessor _contextAccessor;
+        private readonly UserManager<AppUser> _userManager;
 
-        public LayoutService(AppDbContext context, IHttpContextAccessor contextAccessor)
+        public LayoutService(AppDbContext context, IHttpContextAccessor contextAccessor, UserManager<AppUser> userManager)
         {
             _context = context;
             _contextAccessor = contextAccessor;
+            _userManager = userManager;
         }
 
         public async Task<List<BasketVM>> GetBasketsAsync()
         {
-            string cookie = _contextAccessor.HttpContext.Request.Cookies["basket"];
+
 
             List<BasketVM> basketVMs = null;
 
-            if (!string.IsNullOrWhiteSpace(cookie))
+
+            if (_contextAccessor.HttpContext.User.Identity.IsAuthenticated && _contextAccessor.HttpContext.User.IsInRole("Member"))
             {
-                basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(cookie);
+                AppUser appUser = await _userManager.Users
+                    .Include(u => u.Baskets.Where(b => b.IsDeleted == false)).ThenInclude(b=>b.Product)
+                    .FirstOrDefaultAsync(u => u.UserName == _contextAccessor.HttpContext.User.Identity.Name);
+
+                basketVMs = new List<BasketVM>();
+
+                if (appUser.Baskets != null && appUser.Baskets.Count() > 0)
+                {
+
+                    foreach (Basket basket in appUser.Baskets)
+                    {
+                        
+                        BasketVM basketVM = new BasketVM 
+                        {
+                            Id = (int)basket.ProductId,
+                            Count = basket.Count,
+                            ExTax = basket.Product.ExTag,
+                            Image = basket.Product.MainImage,
+                            Price = basket.Product.DiscountedPrice > 0 ? basket.Product.DiscountedPrice: basket.Product.Price,
+                            Title = basket.Product.Title
+                        };
+
+                        basketVMs.Add(basketVM);
+                    }
+
+                }
+
+                string cookie = JsonConvert.SerializeObject(basketVMs);
+                _contextAccessor.HttpContext.Response.Cookies.Append("basket", cookie);
+
             }
             else
             {
-                basketVMs = new List<BasketVM>();
+                string cookie = _contextAccessor.HttpContext.Request.Cookies["basket"];
+
+                if (!string.IsNullOrWhiteSpace(cookie))
+                {
+                    basketVMs = JsonConvert.DeserializeObject<List<BasketVM>>(cookie);
+                }
+                else
+                {
+                    basketVMs = new List<BasketVM>();
+                }
+
+                foreach (BasketVM basketVM in basketVMs)
+                {
+                    Product product = await _context.Products.FirstOrDefaultAsync(p => p.Id == basketVM.Id);
+                    basketVM.Title = product.Title;
+                    basketVM.Image = product.MainImage;
+                    basketVM.Price = product.DiscountedPrice > 0 ? product.DiscountedPrice : product.Price;
+                    basketVM.ExTax = product.ExTag;
+
+                }
             }
 
-            foreach (BasketVM basketVM in basketVMs)
-            {
-                Product product = await _context.Products.FirstOrDefaultAsync(p => p.Id == basketVM.Id);
-                basketVM.Title = product.Title;
-                basketVM.Image = product.MainImage;
-                basketVM.Price = product.DiscountedPrice > 0 ? product.DiscountedPrice : product.Price;
-                basketVM.ExTax = product.ExTag;
-
-            }
+            
 
             return basketVMs;
         }
